c7b5d18ddb824f297d6991ba28080b88e9e80b22
nbonfils <nils.bonfils@epfl.ch>
2020-03-18 07:21:26 +0800

    Backend: Some commenting

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import request from 'request';
 
 const app = express();
 
+// Middleware to avoid the CORS error
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -10,14 +11,18 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
+// The only route that we need to fullfil the requirements
+// This route simply forwards the API request
 app.get('/places/:placeId', (req, res) => {
+  // Get the URL param
   const { placeId } = req.params;
 
+  // Get the eternal API URL based on the place ID
   const url = `https://storage.googleapis.com/coding-session-rest-api/${placeId}`;
 
+  // Forward the request
   request(url, { json: true }, (err, response, body) => {
+    // Forward the response body back
     res.send(body);
   });
 });
